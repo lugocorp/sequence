db52837c28eb5794a53aa4f5c240cd8d89dfd24a
alugocp <alugocp@aim.com>
2023-07-04 22:14:48 -0400

    Medicine Woman's graphic

--- src/game.ts
+++ src/game.ts
@@ -57,7 +57,7 @@ export default class Game {
       time: Time.DAY,
       cave: false
     };
-    this.party.populate('wanderer', 'fisherman', 'gourd elder', 'kamayuq');
+    this.party.populate('fisherman', 'gourd elder', 'kamayuq', 'medicine woman');
     this.chain.futureEvent(new TimeEvent(this), DAY_NIGHT_CYCLE);
   }
 
